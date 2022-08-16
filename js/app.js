f00f9e232c8e489ed478ee910eba2acab5dabd73
sistle <jekich93@gmail.com>
2022-08-17 01:14:27 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -25,45 +25,45 @@ function openCloseMenu(event) {
 
 
 
-new Swiper('.footer-slider', {
-	slidesPerView: 6,
+// new Swiper('.footer-slider', {
+// 	slidesPerView: 6,
 
-	centeredSlides: true,
-	observer: true,
-	observeParents: true,
-	watchOverflow: true,
+// 	centeredSlides: true,
+// 	observer: true,
+// 	observeParents: true,
+// 	watchOverflow: true,
 
-	loop: true,
-	loopAdditionalSlides: 16,
-	speed: 1000,
-	autoplay: {
-		delay: 2000,
-	},
-	simulateTouch: false,
-	spaceBetween: 40,
+// 	loop: true,
+// 	loopAdditionalSlides: 16,
+// 	speed: 1000,
+// 	autoplay: {
+// 		delay: 2000,
+// 	},
+// 	simulateTouch: false,
+// 	spaceBetween: 40,
 
-	breakpoints: {
-		320: {
-			slidesPerView: 2,
-			centeredSlides: false,
-			spaceBetween: 0,
-		},
-		768: {
-			slidesPerView: 2,
-			centeredSlides: true,
+// 	breakpoints: {
+// 		320: {
+// 			slidesPerView: 2,
+// 			centeredSlides: false,
+// 			spaceBetween: 0,
+// 		},
+// 		768: {
+// 			slidesPerView: 2,
+// 			centeredSlides: true,
 			
-		},
-		1024: {
-			slidesPerView: 4,
-			spaceBetween: 15,
-		},
-		1280: {
-			slidesPerView: 6,
-			spaceBetween: 30,
-		},
-	}
+// 		},
+// 		1024: {
+// 			slidesPerView: 4,
+// 			spaceBetween: 15,
+// 		},
+// 		1280: {
+// 			slidesPerView: 6,
+// 			spaceBetween: 30,
+// 		},
+// 	}
 
-})
+// })
 
 
 
